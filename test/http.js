138f968eb219f62389b3d9c6d53027da97123306
David Ellis <d.f.ellis@ieee.org>
2013-01-29 08:01:05 -0800

    Working http transport, now, with its first test.

---
+++ test/http.js
@@ -0,0 +1,32 @@
+var HttpTransport = require('../lib/transports/http');
+var http = require('http');
+
+
+exports.loopback = function(test) {
+    test.expect(2);
+    var httpTransport = new HttpTransport(11235);
+    httpTransport.handler = function(jsonObj, callback) {
+        callback(jsonObj);
+    };
+    var testJSON = JSON.stringify({ hello: 'world' });
+    var req = http.request({
+        hostname: 'localhost',
+        port: 11235,
+        path: '/',
+        method: 'POST'
+    }, function(res) {
+        res.setEncoding('utf8');
+        var resultString = '';
+        res.on('data', function(data) {
+            resultString += data;
+        });
+        res.on('end', function() {
+            test.equal(res.statusCode, 200, 'The http transport provided an OK status code');
+            test.equal(resultString, testJSON, 'The http transport successfully sent the same JSON data back to the client.');
+            httpTransport.server.close();
+            test.done();
+        });
+    });
+    req.write(testJSON);
+    req.end();
+};
