5b94c20c5063f7dcab389f09e51667c426abbda0
David Ellis <d.f.ellis@ieee.org>
2013-01-29 09:34:21 -0800

    Added the second test and reorganized the code a bit.

--- test/http.js
+++ test/http.js
@@ -1,4 +1,4 @@
-var HttpTransport = require('../lib/transports/http');
+var HttpTransport = require('../lib/transports/server/http');
 var http = require('http');
 
 
@@ -23,6 +23,34 @@ exports.loopback = function(test) {
         res.on('end', function() {
             test.equal(res.statusCode, 200, 'The http transport provided an OK status code');
             test.equal(resultString, testJSON, 'The http transport successfully sent the same JSON data back to the client.');
+            httpTransport.server.close();
+            test.done();
+        });
+    });
+    req.write(testJSON);
+    req.end();
+};
+
+exports.failure = function(test) {
+    test.expect(1);
+    var httpTransport = new HttpTransport(12345);
+    httpTransport.handler = function(jsonObj, callback) {
+        callback({ error: "I have no idea what I'm doing." });
+    };
+    var testJSON = JSON.stringify({ hello: 'world' });
+    var req = http.request({
+        hostname: 'localhost',
+        port: 12345,
+        path: '/',
+        method: 'POST'
+    }, function(res) {
+        res.setEncoding('utf8');
+        var resultString = '';
+        res.on('data', function(data) {
+            resultString += data;
+        });
+        res.on('end', function() {
+            test.equal(res.statusCode, 500, 'The http transport provided a server error status code');
             httpTransport.server.close();
             test.done();
         });
