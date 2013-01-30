b924aadc1980efffe998fb42a93eda85aad261b7
David Ellis <d.f.ellis@ieee.org>
2013-01-29 17:36:57 -0800

    Wrote the client http transport and the necessary test.

---
+++ test/client-http.js
@@ -0,0 +1,24 @@
+var HttpTransport = require('../lib/transports/client/http');
+var http = require('http');
+
+exports.loopback = function(test) {
+    test.expect(1);
+    var server = http.createServer(function(req, res) {
+        var buffer = '';
+        req.setEncoding('utf8');
+        req.on('data', function(data) {
+            buffer += data;
+        });
+        req.on('end', function() {
+            res.write(buffer);
+            res.end();
+        });
+    });
+    server.listen(12345);
+    var httpTransport = new HttpTransport('localhost', 12345);
+    httpTransport.request('foo', function(result) {
+        test.equal('foo', result, 'loopback works correctly');
+        server.close();
+        test.done();
+    });
+};
