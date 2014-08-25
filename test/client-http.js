66efdd993669f9334a62293749495437a99c1576
Brian Starke <brian.starke@gmail.com>
2014-08-25 11:14:06 -0400

    on second thought, just add to pre-existing test

--- test/client-http.js
+++ test/client-http.js
@@ -3,8 +3,11 @@ var HttpTransport = jsonrpc.transports.client.http;
 var http = require('http');
 
 exports.loopback = function(test) {
-    test.expect(1);
+    test.expect(3);
     var server = http.createServer(function(req, res) {
+        test.equal('authToken', req.headers.authorization, 'authorization header received');
+        test.equal('thing', req.headers.other, 'other header received');
+
         var buffer = '';
         req.setEncoding('utf8');
         req.on('data', function(data) {
@@ -16,25 +19,6 @@ exports.loopback = function(test) {
         });
     });
     server.listen(12345, 'localhost', function() {
-        var httpTransport = new HttpTransport('localhost', 12345);
-        httpTransport.request('foo', function(result) {
-            test.equal('foo', result, 'loopback works correctly');
-            server.close();
-            test.done();
-        });
-    });
-};
-
-exports.headers = function(test) {
-    test.expect(2);
-    var server = http.createServer(function(req) {
-        test.equal('authToken', req.headers.authorization, 'authorization header received');
-        test.equal('thing', req.headers.other, 'other header received');
-
-        server.close();
-        test.done();
-    });
-    server.listen(12345, 'localhost', function() {
         var options = {
             headers: {
                 authorization: 'authToken',
@@ -43,6 +27,10 @@ exports.headers = function(test) {
         };
 
         var httpTransport = new HttpTransport('localhost', 12345, options);
-        httpTransport.request('foo');
+        httpTransport.request('foo', function(result) {
+            test.equal('foo', result, 'loopback works correctly');
+            server.close();
+            test.done();
+        });
     });
 };
