f2ec964dac314e9f6f4313e0611cd1b4319a2d52
Brian Starke <brian.starke@gmail.com>
2014-08-25 10:20:54 -0400

    added unit test for headers

--- test/client-http.js
+++ test/client-http.js
@@ -24,3 +24,25 @@ exports.loopback = function(test) {
         });
     });
 };
+
+exports.headers = function(test) {
+    test.expect(2);
+    var server = http.createServer(function(req) {
+        test.equal('authToken', req.headers.authorization, 'authorization header received');
+        test.equal('thing', req.headers.other, 'other header received');
+
+        server.close();
+        test.done();
+    });
+    server.listen(12345, 'localhost', function() {
+        var options = {
+            headers: {
+                authorization: 'authToken',
+                other: 'thing'
+            }
+        };
+
+        var httpTransport = new HttpTransport('localhost', 12345, options);
+        httpTransport.request('foo');
+    });
+};
